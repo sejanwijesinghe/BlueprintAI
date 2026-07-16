426a1ba94a2619dc6692b079af578feac4b09b2c
Sejan Wijesinghe <sejanwijesinghe@gmail.com>
2026-07-16 14:57:08 +0530

    implement home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
+import Button from "../../components/ui/Button";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -11,9 +13,90 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-      <div className="Home">
+      <div className="home">
         <Navbar/>
-        <h1 className="text-3xl text-indigo-700 font-semibold">home</h1>;
+        <section className="hero">
+            <div className={"announce"}>
+                <div className={"dot"}>
+                    <div className={"pulse"}></div>
+                </div>
+                <p>Introducing BlueprintAI v 1.0</p>
+
+            </div>
+            <h1>
+                Build Full 3D Models From 2D Blueprints Instantly
+
+            </h1>
+            <p className="subtitle">
+                Upload your PDF, DWG, or image plans. Our spatial AI engines analyze line weights, walls, and structural layers to extrude precise, high-fidelity 3D environments in seconds.
+            </p>
+
+            <div className="actions">
+                <a href="#upload" className="cta">
+                    Build Now <ArrowRight className="icon"/>
+                </a>
+                <Button variant="outline" size="lg" className="demo">
+                    How Works
+                </Button>
+            </div>
+            <div id="upload" className="upload-shell">
+                <div className="grid-overlay"></div>
+
+                <div className="upload-card">
+                    <div className="upload-head">
+                        <div className="upload-icon">
+                            <Layers className="icon" />
+                        </div>
+
+                        <h3>Upload your floor plan</h3>
+                        <p>Supports JPG, PNG formats up to 10MB</p>
+                    </div>
+                    <p>Upload images</p>
+                </div>
+            </div>
+
+        </section>
+
+          <section className="projects">
+              <div className="section-inner">
+                  <div className="section-head">
+                      <div className="copy">
+                          <h2>Projects</h2>
+                          <p>Your latest work and shared community projects, all in one place.</p>
+                      </div>
+                  </div>
+
+                  <div className="projects-grid">
+                      {
+                          <div className="project-card group">
+                              <div className="preview">
+                                  <img  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project"
+                                  />
+
+                                  <div className="badge">
+                                      <span>Community</span>
+                                  </div>
+                              </div>
+
+                              <div className="card-body">
+                                  <div>
+                                      <h3>Project Manhattam</h3>
+
+                                      <div className="meta">
+                                          <Clock size={12} />
+                                          <span>{new Date('01.01.2026').toLocaleDateString()}</span>
+                                          <span>By JS Mastery</span>
+                                      </div>
+                                  </div>
+                                  <div className="arrow">
+                                      <ArrowUpRight size={18} />
+                                  </div>
+                              </div>
+                          </div>
+                      }
+                  </div>
+              </div>
+          </section>
       </div>
   )
 }
