05caf6c5dee92654cebcab2622fc027b4da662f1
Sejan Wijesinghe <sejanwijesinghe@gmail.com>
2026-07-17 10:29:35 +0530

    Final Fix

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,8 +9,8 @@ import {createProject, getProjects} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        { title: "New React Router App" },
-        { name: "description", content: "Welcome to React Router!" },
+        { title: "BlueprintAI" },
+        { name: "description", content: "Welcome to BlueprintAI!" },
     ];
 }
 
